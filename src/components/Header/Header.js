3cf352224882c9247784a2beb3840f118df2a95e
lingardium <karatascanahmet@yahoo.com>
2022-09-11 21:01:01 +0000

    final

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,24 +7,24 @@ import classes from "./Header.module.scss";
 const Header = () => {
   return (
     <Fragment>
-      <header className={classes.header}>  
+      <header className={classes.header}>
         <div>
           <h1>Age of Empires Wiki</h1>
         </div>
         <nav className={classes.nav}>
-        <ul>
-          <li>
-            <NavLink to='/' activeClassName={classes.active}>
-            Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink to='/units' activeClassName={classes.active}>
-            Units
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
+          <ul>
+            <li>
+              <NavLink to="/" activeClassName={classes.active}>
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/units" activeClassName={classes.active}>
+                Units
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
       </header>
       <div className={classes["main-image"]}>
         <img src={ageofempiresImage} alt="Age of empires cover" />
--- src/store/sagas/handlers/age.js
+++ src/store/sagas/handlers/age.js
@@ -1,6 +1,6 @@
 import { put } from "redux-saga/effects";
 
 export default function* ageFilterHandler(action) {
-  yield put({type: 'AGE', payload: action.payload});
-  yield put({type: 'FILTER'});
+  yield put({ type: "AGE", payload: action.payload });
+  yield put({ type: "FILTER" });
 }
